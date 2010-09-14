9e8804b1dfb2959af95b81233e7e98588e0e02dd
Guillaume Mary <guillaume.mary@gmail.com>
2010-09-14 16:23:46 +0000

    Add comment and upgrade the implementation to fit the new agent interface

--- appstatus-log/src/main/java/net/sf/appstatus/agent/service/impl/LogServiceMonitorAgent.java
+++ appstatus-log/src/main/java/net/sf/appstatus/agent/service/impl/LogServiceMonitorAgent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Capgemini
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ */
 package net.sf.appstatus.agent.service.impl;
 
 import java.util.UUID;
@@ -7,6 +22,11 @@ import net.sf.appstatus.agent.service.IServiceMonitorAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Service monitor agent who logs the operation calls.
+ * @author Guillaume Mary
+ *
+ */
 public class LogServiceMonitorAgent implements IServiceMonitorAgent {
 
 	private static Logger log = LoggerFactory
@@ -15,18 +35,32 @@ public class LogServiceMonitorAgent implements IServiceMonitorAgent {
 	private String executionId;
 
 	private String serviceName;
-
-	public String beginCall(String serviceName, Object[] parameters) {
+	
+	/**
+	 * Default constructor.
+	 * @param serviceName
+	 */
+	public LogServiceMonitorAgent(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String beginCall(String operationName, Object[] parameters) {
 		this.executionId = UUID.randomUUID().toString();
 		log.info(
-				"Debut de l'appel du service <{}({})>, avec les parametres : {}",
-				new Object[] { serviceName, executionId, parameters });
+				"Call service <{}.{} ({})>, with these parameters : {}",
+				new Object[] { serviceName, operationName, executionId, parameters });
 		return executionId;
 	}
-
-	public void endCall(String id) {
-		log.info("Fin de l'appel du service <{}({})>", new Object[] {
-				serviceName, executionId });
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void endCall(String operationName, String executionId) {
+		log.info("End of the call of the service <{}.{} ({})>", new Object[] {
+				serviceName, operationName, executionId });
 	}
 
 }
