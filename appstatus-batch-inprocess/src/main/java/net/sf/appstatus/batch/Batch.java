0277f0b907661ba1c3aaec82450c333e666086bf
Nicolas Richeton <nicolas.richeton@gmail.com>
2011-05-10 10:26:00 +0000

    MAJ Spring

--- appstatus-batch-inprocess/src/main/java/net/sf/appstatus/batch/Batch.java
+++ appstatus-batch-inprocess/src/main/java/net/sf/appstatus/batch/Batch.java
@@ -8,43 +8,51 @@ import net.sf.appstatus.core.batch.IBatch;
 import net.sf.appstatus.core.batch.IBatchProgressMonitor;
 
 public class Batch implements IBatch {
-	String uuid;
-	String name;
-	String group;
 	Date endDate;
-	Date startDate = new Date();
+	String group;
 	InProcessBatchProgressMonitor monitor;
+	String name;
+	Date startDate = new Date();
+	String uuid;
 
-
-	public String getUuid() {
-		return uuid;
+	public String getCurrentItem() {
+		return monitor.getCurrentItem().toString();
 	}
 
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
+	public String getCurrentTask() {
+		return monitor.getTaskName();
 	}
 
 	public Date getEndDate() {
 		return monitor.getEstimateEndDate();
 	}
 
+	public String getGroup() {
+		return group;
+	}
+
 	public String getLastMessage() {
 		return monitor.getLastMessage();
 	}
 
-	public String getCurrentTask(){
-		return monitor.getTaskName();
+	public String getName() {
+		return name;
 	}
-	public float getProgressStatus() {
-		if (monitor == null || monitor.getTotalWork() <= 0)
-			return -1;
 
-		return  monitor.getProgress() * 100f
-				/ (float) monitor.getTotalWork();
+	public IBatchProgressMonitor getProgressMonitor() {
+		return monitor;
+	}
+
+	public float getProgressStatus() {
+		if (monitor == null || monitor.getTotalWork() <= 0) {
+			return -1;
+		}
+
+		return monitor.getProgress() * 100f / monitor.getTotalWork();
 	}
 
 	public List<String> getRejectedItemsId() {
-	
+
 		return new ArrayList<String>();
 	}
 
@@ -53,38 +61,28 @@ public class Batch implements IBatch {
 	}
 
 	public String getStatus() {
-		return monitor.isDone()?"Completed":"Running";
+		return monitor.isDone() ? "Completed" : "Running";
 	}
 
-	public String getGroup() {
-		return group;
-	}
-
-	public String getName() {
-		return name;
+	public String getUuid() {
+		return uuid;
 	}
 
 	public void setGroup(String group2) {
 		this.group = group2;
-		
+
 	}
 
 	public void setName(String name2) {
-	this.name = name2;
+		this.name = name2;
 	}
-
-	public String getCurrentItem() {
-		return monitor.getCurrentItem().toString();
-	}
-
-
 
 	public void setProgressMonitor(IBatchProgressMonitor monitor) {
-		this.monitor = (InProcessBatchProgressMonitor)monitor;
+		this.monitor = (InProcessBatchProgressMonitor) monitor;
 	}
 
-	public IBatchProgressMonitor getProgressMonitor() {
-		return monitor;
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
 	}
- 
+
 }
