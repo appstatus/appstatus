7863e70b148c901dcfc9ea991c998842a975519a
Nicolas Richeton <nicolas.richeton@gmail.com>
2012-04-01 17:07:55 +0000

    https://sourceforge.net/apps/mantisbt/appstatus/view.php?id=35

--- appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/AppStatusServices.java
+++ appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/AppStatusServices.java
@@ -28,7 +28,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This is the entry point for services monitor..
+ * This is the entry point for services monitor.
  * 
  * <p>
  * Must be initialized once before calling other methods.
@@ -43,150 +43,156 @@ import org.slf4j.LoggerFactory;
 @Deprecated
 public class AppStatusServices {
 
-    private static Logger logger = LoggerFactory.getLogger(AppStatusServices.class);
+	private static Logger logger = LoggerFactory
+			.getLogger(AppStatusServices.class);
 
-    private boolean initDone = false;
-    private IObjectInstantiationListener objectInstanciationListener = null;
-    private IServiceManager serviceManager = null;
-    private IServletContextProvider servletContextProvider = null;
+	private boolean initDone = false;
+	private IObjectInstantiationListener objectInstanciationListener = null;
+	private IServiceManager serviceManager = null;
+	private IServletContextProvider servletContextProvider = null;
 
-    /**
-     * Status Service creator.
-     */
-    public AppStatusServices() {
-    }
+	/**
+	 * Status Service creator.
+	 */
+	public AppStatusServices() {
+	}
 
-    private void checkInit() {
-        if (!initDone) {
-            logger.warn("Not initialized. Starting init");
-            init();
+	private void checkInit() {
+		if (!initDone) {
+			logger.warn("Not initialized. Starting init");
+			init();
 
-        }
-    }
+		}
+	}
 
-    /**
-     * Try to instantiate a class.
-     * 
-     * @param className
-     * @return an object instance of "className" class or null if instantiation
-     *         is not possible
-     */
-    private Object getClassInstance(String className) {
-        Object obj = null;
+	/**
+	 * Try to instantiate a class.
+	 * 
+	 * @param className
+	 * @return an object instance of "className" class or null if instantiation
+	 *         is not possible
+	 */
+	private Object getClassInstance(String className) {
+		Object obj = null;
 
-        if (objectInstanciationListener != null) {
-            obj = objectInstanciationListener.getInstance(className);
-        }
+		if (objectInstanciationListener != null) {
+			obj = objectInstanciationListener.getInstance(className);
+		}
 
-        if (obj == null) {
-            try {
-                obj = Class.forName(className).newInstance();
-            } catch (ClassNotFoundException e) {
-                logger.warn("Class {} not found ", className, e);
-            } catch (InstantiationException e) {
-                logger.warn("Cannot instanciate {} ", className, e);
-            } catch (IllegalAccessException e) {
-                logger.warn("Cannot access class {} for instantiation ", className, e);
-            }
-        }
+		if (obj == null) {
+			try {
+				obj = Class.forName(className).newInstance();
+			} catch (ClassNotFoundException e) {
+				logger.warn("Class {} not found ", className, e);
+			} catch (InstantiationException e) {
+				logger.warn("Cannot instanciate {} ", className, e);
+			} catch (IllegalAccessException e) {
+				logger.warn("Cannot access class {} for instantiation ",
+						className, e);
+			}
+		}
 
-        if (obj != null) {
-            injectServletContext(obj);
-        }
+		if (obj != null) {
+			injectServletContext(obj);
+		}
 
-        return obj;
-    }
+		return obj;
+	}
 
-    public IServiceMonitor getServiceMonitor(String name, String group) {
-        checkInit();
+	public IServiceMonitor getServiceMonitor(String name, String group) {
+		checkInit();
 
-        IService batch = null;
-        if (serviceManager != null) {
-            batch = serviceManager.getService(name, group);
-            return serviceManager.getMonitor(batch);
-        }
+		IService batch = null;
+		if (serviceManager != null) {
+			batch = serviceManager.getService(name, group);
+			return serviceManager.getMonitor(batch);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public List<IService> getServices() {
-        return serviceManager.getServices();
-    }
+	public List<IService> getServices() {
+		return serviceManager.getServices();
+	}
 
-    public IServletContextProvider getServletContext() {
-        return servletContextProvider;
-    }
+	public IServletContextProvider getServletContext() {
+		return servletContextProvider;
+	}
 
-    public synchronized void init() {
+	public synchronized void init() {
 
-        if (initDone) {
-            logger.warn("Already initialized");
-            return;
-        }
+		if (initDone) {
+			logger.warn("Already initialized");
+			return;
+		}
 
-        try {
+		try {
 
-            // Load plugins
-            loadPlugins();
-        } catch (Exception e) {
-            logger.error("Initialization error", e);
-        }
+			// Load plugins
+			loadPlugins();
+		} catch (Exception e) {
+			logger.error("Initialization error", e);
+		}
 
-        initDone = true;
+		initDone = true;
 
-    }
+	}
 
-    private void injectServletContext(Object instance) {
-        // Inject servlet context if possible
-        if (instance instanceof IServletContextAware && servletContextProvider != null) {
-            ((IServletContextAware) instance).setServletContext(servletContextProvider.getServletContext());
-        }
-    }
+	private void injectServletContext(Object instance) {
+		// Inject servlet context if possible
+		if (instance instanceof IServletContextAware
+				&& servletContextProvider != null) {
+			((IServletContextAware) instance)
+					.setServletContext(servletContextProvider
+							.getServletContext());
+		}
+	}
 
-    private void loadPlugins() {
-        try {
-            Enumeration<URL> plugins = AppStatusServices.class.getClassLoader().getResources(
-                    "net/sf/appstatus/plugin.properties");
-            while (plugins.hasMoreElements()) {
-                URL url = plugins.nextElement();
-                logger.info(url.toString());
-                Properties p = loadProperties(url);
+	private void loadPlugins() {
+		try {
+			Enumeration<URL> plugins = AppStatusServices.class.getClassLoader()
+					.getResources("net/sf/appstatus/plugin.properties");
+			while (plugins.hasMoreElements()) {
+				URL url = plugins.nextElement();
+				logger.info(url.toString());
+				Properties p = loadProperties(url);
 
-                // serviceManager
-                String serviceManagerClass = p.getProperty("serviceManager");
-                if (serviceManagerClass != null) {
-                    serviceManager = (IServiceManager) getClassInstance(serviceManagerClass);
-                }
+				// serviceManager
+				String serviceManagerClass = p.getProperty("serviceManager");
+				if (serviceManagerClass != null) {
+					serviceManager = (IServiceManager) getClassInstance(serviceManagerClass);
+				}
 
-            }
-        } catch (IOException e) {
-            logger.warn("Error loading plugins", e);
-        }
-    }
+			}
+		} catch (IOException e) {
+			logger.warn("Error loading plugins", e);
+		}
+	}
 
-    /**
-     * Load a properties file from a given URL.
-     * 
-     * @param url
-     *            an url
-     * @return a {@link Properties} object
-     * @throws IOException
-     *             in an error occurs
-     */
-    private Properties loadProperties(URL url) throws IOException {
-        // Load plugin configuration
-        Properties p = new Properties();
-        InputStream is = url.openStream();
-        p.load(is);
-        is.close();
-        return p;
-    }
+	/**
+	 * Load a properties file from a given URL.
+	 * 
+	 * @param url
+	 *            an url
+	 * @return a {@link Properties} object
+	 * @throws IOException
+	 *             in an error occurs
+	 */
+	private Properties loadProperties(URL url) throws IOException {
+		// Load plugin configuration
+		Properties p = new Properties();
+		InputStream is = url.openStream();
+		p.load(is);
+		is.close();
+		return p;
+	}
 
-    public void setObjectInstanciationListener(IObjectInstantiationListener objectInstanciationListener) {
-        this.objectInstanciationListener = objectInstanciationListener;
-    }
+	public void setObjectInstanciationListener(
+			IObjectInstantiationListener objectInstanciationListener) {
+		this.objectInstanciationListener = objectInstanciationListener;
+	}
 
-    public void setServletContextProvider(IServletContextProvider servletContext) {
-        this.servletContextProvider = servletContext;
-    }
+	public void setServletContextProvider(IServletContextProvider servletContext) {
+		this.servletContextProvider = servletContext;
+	}
 }
