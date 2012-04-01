d97421fd3ed07a7feefd02042c3e775711f41eab
Nicolas Richeton <nicolas.richeton@gmail.com>
2012-04-01 16:50:54 +0000

    http://sourceforge.net/apps/mantisbt/appstatus/view.php?id=17 First json impl.

--- appstatus/appstatus-web/src/main/java/net/sf/appstatus/web/pages/ServicesPage.java
+++ appstatus/appstatus-web/src/main/java/net/sf/appstatus/web/pages/ServicesPage.java
@@ -13,46 +13,53 @@ import net.sf.appstatus.core.services.IService;
 import net.sf.appstatus.web.HtmlUtils;
 
 public class ServicesPage extends AbstractPage {
-    private static final String ENCODING = "UTF-8";
+	private static final String ENCODING = "UTF-8";
 
-    @Override
-    public void doGet(AppStatus status, HttpServletRequest req, HttpServletResponse resp)
-            throws UnsupportedEncodingException, IOException {
+	@Override
+	public void doGet(AppStatus status, HttpServletRequest req,
+			HttpServletResponse resp) throws UnsupportedEncodingException,
+			IOException {
 
-        setup(resp);
-        ServletOutputStream os = resp.getOutputStream();
-        begin(os);
+		setup(resp, "text/html");
+		ServletOutputStream os = resp.getOutputStream();
+		begin(os);
 
-        List<IService> services = status.getServices();
+		List<IService> services = status.getServices();
 
-        os.write("<h1>Services</h1>".getBytes(ENCODING));
+		os.write("<h1>Services</h1>".getBytes(ENCODING));
 
-        if (HtmlUtils.generateBeginTable(os, services.size())) {
+		if (HtmlUtils.generateBeginTable(os, services.size())) {
 
-            HtmlUtils.generateHeaders(os, "", "Category", "Name", "Hits", "Cache", "Running", "min", "max", "avg",
-                    "min (cached)", "max (cached)", "avg (cached");
+			HtmlUtils.generateHeaders(os, "", "Category", "Name", "Hits",
+					"Cache", "Running", "min", "max", "avg", "min (cached)",
+					"max (cached)", "avg (cached");
 
-            for (IService service : services) {
-                HtmlUtils.generateRow(os, Icons.STATUS_JOB, service.getGroup(), service.getName(), service.getHits(),
-                        service.getCacheHits(), service.getRunning(), service.getMinResponseTime(),
-                        service.getMaxResponseTime(), Math.round(service.getAvgResponseTime()),
-                        service.getMinResponseTimeWithCache(), service.getMaxResponseTimeWithCache(),
-                        Math.round(service.getAvgResponseTimeWithCache()));
-            }
+			for (IService service : services) {
+				HtmlUtils.generateRow(os, Icons.STATUS_JOB, service.getGroup(),
+						service.getName(), service.getHits(),
+						service.getCacheHits(), service.getRunning(),
+						service.getMinResponseTime(),
+						service.getMaxResponseTime(),
+						Math.round(service.getAvgResponseTime()),
+						service.getMinResponseTimeWithCache(),
+						service.getMaxResponseTimeWithCache(),
+						Math.round(service.getAvgResponseTimeWithCache()));
+			}
 
-            HtmlUtils.generateEndTable(os, services.size());
-        }
-        end(os);
-    }
+			HtmlUtils.generateEndTable(os, services.size());
+		}
+		end(os);
+	}
 
-    @Override
-    public void doPost(AppStatus status, HttpServletRequest req, HttpServletResponse resp) {
-        // TODO Auto-generated method stub
+	@Override
+	public void doPost(AppStatus status, HttpServletRequest req,
+			HttpServletResponse resp) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public String getId() {
-        return "services";
-    }
+	@Override
+	public String getId() {
+		return "services";
+	}
 }
