a8d34fce116127d47ce11ea7dae8d8e217881974
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-01-08 16:34:07 +0000

    Javadoc

--- appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/services/IServiceManager.java
+++ appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/services/IServiceManager.java
@@ -10,10 +10,33 @@ import java.util.List;
  */
 public interface IServiceManager {
 
-	IServiceMonitor getMonitor(IService batch);
+	/**
+	 * Returns a monitor for a new service call.
+	 * 
+	 * @param service
+	 * @return
+	 */
+	IServiceMonitor getMonitor(IService service);
 
+	/**
+	 * Create or get the service definition identified by group and name.
+	 * 
+	 * @param name
+	 *            name of the service.
+	 * @param group
+	 *            group of the service.
+	 * @return
+	 */
 	IService getService(String name, String group);
 
+	/**
+	 * List all current services.
+	 * 
+	 * <p>
+	 * Services are defined using #getService(String, String)
+	 * 
+	 * @return
+	 */
 	List<IService> getServices();
 
 }
