9a7f9433a12985ac47b9c2eaa4b3a7722dbc54de
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-01-09 17:01:34 +0000

    Support for Spring Cache (with useThreadLocal)

---
+++ appstatus/appstatus-core/src/main/java/net/sf/appstatus/support/cache/AppStatusCacheAdapter.java
@@ -0,0 +1,54 @@
+package net.sf.appstatus.support.cache;
+
+import net.sf.appstatus.core.services.IServiceMonitor;
+import net.sf.appstatus.core.services.ServiceMonitorLocator;
+
+import org.springframework.cache.Cache;
+
+/**
+ * A simple adapter for Spring Caches with flags the current service call as
+ * "cacheHit".
+ * 
+ * @author Nicolas Richeton
+ * 
+ */
+public class AppStatusCacheAdapter implements Cache {
+
+	Cache cache;
+
+	public AppStatusCacheAdapter(Cache ehcache) {
+		this.cache = ehcache;
+	}
+
+	public void clear() {
+		cache.clear();
+	}
+
+	public void evict(Object key) {
+		cache.evict(key);
+	}
+
+	public ValueWrapper get(Object key) {
+		ValueWrapper result = cache.get(key);
+
+		if (result != null) {
+			IServiceMonitor monitor = ServiceMonitorLocator.getCurrentServiceMonitor();
+			if (monitor != null) {
+				monitor.cacheHit();
+			}
+		}
+		return result;
+	}
+
+	public String getName() {
+		return cache.getName();
+	}
+
+	public Object getNativeCache() {
+		return cache.getNativeCache();
+	}
+
+	public void put(Object key, Object value) {
+		cache.put(key, value);
+	}
+}
