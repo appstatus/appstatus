98816a378e33910e2784a85fa7f6f43dd2cb78d3
Nicolas Richeton <nicolas.richeton@gmail.com>
2014-09-04 09:30:40 +0000

    Fix init with partial spring configuration

--- appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/AppStatus.java
+++ appstatus/appstatus-core/src/main/java/net/sf/appstatus/core/AppStatus.java
@@ -291,6 +291,10 @@ public class AppStatus {
 			}
 		}
 
+		// If configuration is null (Spring with no configuration block), create
+		// empty object
+		configuration = new Properties();
+
 		// Give all configuration properties to managers.
 		if (getBatchManager() != null) {
 			Properties newConfiguration = getBatchManager().getConfiguration();
